64fae4fb57ccf9d7fe563df353ea46d0ca68c004
herman-gg <hrmn.mxgn@gmail.com>
2025-09-12 20:15:10 +0200

    feat(test): Add test

---
+++ main_test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountWords(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4")
+
+	expected := 4
+	actual := count(b)
+
+	if actual != expected {
+		t.Errorf("Expected [%d], got %d instead.\n", expected, actual)
+	}
+}
