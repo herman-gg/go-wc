f0dcb0449591593b3161e94f5c27b14eabefdd7c
herman-gg <hrmn.mxgn@gmail.com>
2025-09-14 17:30:35 +0200

    feat(flags): Add flags and tests;

--- main.go
+++ main.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
 	"io"
+	"os"
 )
 
-func main() {
-
+type Flags struct {
+	isForLines bool
+	isForBytes bool
 }
 
-func count(r io.Reader) int {
+func main() {
+	isForLines := flag.Bool("l", false, "Count lines")
+	isForBytes := flag.Bool("b", false, "Count bytes")
+	flag.Parse()
+
+	flags := Flags{*isForLines, *isForBytes}
+
+	fmt.Println(count(os.Stdin, flags))
+}
+
+func count(r io.Reader, flags Flags) int {
 	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
+
+	splitFunc := bufio.ScanWords
+	if flags.isForLines {
+		splitFunc = bufio.ScanLines
+	} else if flags.isForBytes {
+		splitFunc = bufio.ScanBytes
+	}
+
+	scanner.Split(splitFunc)
 
 	wc := 0
 
--- main_test.go
+++ main_test.go
@@ -9,9 +9,31 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4")
 
 	expected := 4
-	actual := count(b)
+	actual := count(b, Flags{isForLines: false, isForBytes: false})
 
 	if actual != expected {
-		t.Errorf("Expected [%d], got %d instead.\n", expected, actual)
+		t.Errorf("Expected [%d] words, got %d instead.\n", expected, actual)
+	}
+}
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("line1\nline2\nlin3")
+
+	expected := 3
+	actual := count(b, Flags{isForLines: true, isForBytes: false})
+
+	if actual != expected {
+		t.Errorf("Expected [%d] lines, got %d instead.\n", expected, actual)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("fox")
+
+	expected := 3
+	actual := count(b, Flags{isForLines: false, isForBytes: true})
+
+	if actual != expected {
+		t.Errorf("Expected [%d] bytes, got %d instead.\n", expected, actual)
 	}
 }
